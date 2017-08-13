7402fb1d5d03fbb9f978cfdf64b9787f65f1b110
Lukas Winkler <l.winkler23@mailbox.org>
2017-08-13 20:23:09 +0200

    fix icons

--- js/devtools.js
+++ js/devtools.js
@@ -2,6 +2,6 @@ var port = chrome.runtime.connect({
     name: "devtool-comunication" //Given a Name
 });
 
-chrome.devtools.panels.create("Piwik Checker", "images/devtoolicon.png", "views/panel.html", function(panel) {
+chrome.devtools.panels.create("Piwik Checker", "images/icon48.png", "views/panel.html", function(panel) {
     console.log("panel loaded");
 });
